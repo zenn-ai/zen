52e92b3b4b6e40ce4eadd3a496899b9420c1b843
Manish Kumar <sihagmanish36@gmail.com>
2023-11-05 04:03:25 +0000

    Updated training script to include alexander street data

--- train/v2/finetune_deepspeed_lora.py
+++ train/v2/finetune_deepspeed_lora.py
@@ -22,40 +22,79 @@ from transformers.integrations import deepspeed
 
 from peft import LoraConfig, get_peft_model
 
+from fastchat.conversation import get_conv_template, register_conv_template, Conversation, SeparatorStyle
+
+
 IGNORE_TOKEN_ID = LabelSmoother.ignore_index
+SYSTEM_MSG = """Your name is ZenAI and you're a therapist. Please have a conversation with your patient and provide them with a helpful response to their concerns."""
 
-csv_files = ["mental_health_chatbot_dataset.csv", "psychology-dataset.csv", "who_r_u.csv"]
-df = pd.DataFrame()
-
-for p in csv_files:
-    df1 = pd.read_csv(f"../../data/processed/{p}")[["human", "zen"]]
-    df1 = df1.rename(columns={"human": "USER", "zen": "ASSISTANT"})
-    df1 = df1.drop_duplicates(subset=["USER", "ASSISTANT"], keep="first", ignore_index=True)
-
-    df1["USER"] = df1.USER.str.replace('\s+', ' ', regex=True)
-    df1["USER"] = df1.USER.str.replace(r'\.([a-zA-Z0-9])', r'. \1', regex=True)
-    df1["ASSISTANT"] = df1.ASSISTANT.str.replace('\s+', ' ', regex=True)
-    df1["ASSISTANT"] = df1.ASSISTANT.str.replace(r'\.([a-zA-Z0-9])', r'. \1', regex=True)
-    
-    df = pd.concat([df, df1])
-
-df = df.sample(frac=1, random_state=42)
-train, test = train_test_split(df, test_size=1000, random_state=42)
-print(df.shape, train.shape, test.shape)
-
-def format_example(df):
-    instruction = (
-        "You are a licensed therapist. Please have a conversation with your patient and provide them with a helpful response to their concerns."
+try:
+    register_conv_template(
+        Conversation(
+            name="ZenAI",
+            system_message=SYSTEM_MSG,
+            roles=("USER", "ASSISTANT"),
+            sep_style=SeparatorStyle.ADD_COLON_TWO,
+            sep=" ",
+            sep2="</s>",
+        )
     )
-    p_header = "USER:"
-    z_header = "ASSISTANT:"
+except AssertionError:
+    pass
 
-    p = df["USER"]
-    z = df["ASSISTANT"]
 
-    df["text"] = f"{instruction} {p_header} {p} {z_header} {z}</s>"
+def get_df():
+    csv_files = ["mental_health_chatbot_dataset.csv", "psychology-dataset.csv", "who_r_u.csv"]
+    df = pd.DataFrame()
 
+    for p in csv_files:
+        df1 = pd.read_csv(f"../../data/processed/{p}")[["human", "zen"]]
+        df1 = df1.rename(columns={"human": "USER", "zen": "ASSISTANT"})
+        df1 = df1.drop_duplicates(subset=["USER", "ASSISTANT"], keep="first", ignore_index=True)
+
+        df1["USER"] = df1.USER.str.replace('\s+', ' ', regex=True)
+        df1["USER"] = df1.USER.str.replace(r'\.([a-zA-Z0-9])', r'. \1', regex=True)
+        df1["ASSISTANT"] = df1.ASSISTANT.str.replace('\s+', ' ', regex=True)
+        df1["ASSISTANT"] = df1.ASSISTANT.str.replace(r'\.([a-zA-Z0-9])', r'. \1', regex=True)
+
+        df = pd.concat([df, df1])
+
+    df = df.sample(frac=1, random_state=42)
     return df
+
+
+def get_conversations(df, reset):
+    conv = get_conv_template("ZenAI")
+    conversations = []
+    
+    conv.messages = []
+    for index, row in df.iterrows():
+        if reset:
+            conv.messages = []
+        conv.append_message("USER", row["USER"])
+        conv.append_message("ASSISTANT", row["ASSISTANT"])
+        conversations.append(conv.get_prompt())
+    
+    return conversations
+
+
+def get_therapy_conv():
+    df = pd.read_csv("../../data/processed/PALM_Alexander_Street.csv")
+    df = df.dropna(ignore_index=True)
+    df = df.drop(index=0)
+    df.reset_index(drop=True, inplace=True)
+    
+    conv_ids = df.conv_id.unique()
+    conversations = []
+    for c in conv_ids:
+        conv_df = df[df.conv_id == c]
+        conversations += get_conversations(conv_df, reset=False)
+    
+    return conversations
+
+
+def rank0_print(*args):
+    print(*args)
 
 
 def preprocess(conversations, tokenizer):
@@ -117,6 +156,13 @@ def preprocess(conversations, tokenizer):
                     f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
                     f" #turn = {len(turns) - 1}. (ignored)"
                 )
+                print(conversation)
+                print(input_ids)
+                print(target)
+                z = target.clone()
+                z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
+                rank0_print(tokenizer.decode(z))
+                print("==================================================================")
 
     return dict(
         input_ids=input_ids,
@@ -138,10 +184,9 @@ tokenizer.pad_token = tokenizer.unk_token
 class SupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, df, tokenizer: PreTrainedTokenizer):
+    def __init__(self, conversations, tokenizer: PreTrainedTokenizer):
         super(SupervisedDataset, self).__init__()
 
-        conversations = df.apply(format_example, axis=1).text.tolist()
         data_dict = preprocess(conversations, tokenizer)
 
         self.input_ids = data_dict["input_ids"]
@@ -159,14 +204,21 @@ class SupervisedDataset(Dataset):
         )
 
 
+df = get_df()
+conversations = get_conversations(df, reset=True)
+conversations += get_therapy_conv()
+
+train, test = train_test_split(conversations, test_size=1000, random_state=42)
+print(len(conversations), len(train), len(test))
+
 train_dataset = SupervisedDataset(train, tokenizer)
-# eval_dataset = SupervisedDataset(test, tokenizer)
-# data_module = dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
+eval_dataset = SupervisedDataset(test, tokenizer)
+data_module = dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
 
 train_args = TrainingArguments(
-    output_dir="/home/jupyter/therapy-bot/models/zenai_sample",
-    resume_from_checkpoint=True,
-    max_steps=5000,
+    output_dir="/home/jupyter/therapy-bot/models/zenai_sample2",
+    resume_from_checkpoint=False,
+    max_steps=50,
     optim="adamw_torch",
     per_device_train_batch_size=1,
     remove_unused_columns=False,
@@ -175,21 +227,21 @@ train_args = TrainingArguments(
     gradient_accumulation_steps=1,
     fp16=True,
     lr_scheduler_type="constant_with_warmup",
-    warmup_steps=200,
-    save_steps=500,
+    warmup_steps=2,
+    save_steps=10,
     save_strategy="steps",
-    # evaluation_strategy="steps",
-    # eval_steps=50,
+    evaluation_strategy="steps",
+    eval_steps=10,
     save_total_limit=1,
     logging_steps=10,
     report_to="tensorboard",
-    hub_model_id="kmnis/ZenAI-v2",
-    hub_strategy="checkpoint",
-    hub_private_repo=True,
-    # load_best_model_at_end=True,
-    push_to_hub=True,
+    # hub_model_id="kmnis/ZenAI-v3",
+    # hub_strategy="checkpoint",
+    # hub_private_repo=True,
+    load_best_model_at_end=True,
+    # push_to_hub=True,
     deepspeed="ds_config_zero3.json",
-    hub_token="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"
+    # hub_token="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"
 )
 
 lora_r = 8
@@ -222,7 +274,7 @@ model.print_trainable_parameters()
 
 # creating trainer
 trainer = Trainer(
-    model=model, tokenizer=tokenizer, args=train_args, train_dataset=train_dataset
+    model=model, tokenizer=tokenizer, args=train_args, **data_module
 )
 model.config.use_cache = False
 
