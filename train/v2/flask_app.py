2abc82d5b424a9960f60a68774361b770e30b2a8
Manish Kumar <sihagmanish36@gmail.com>
2023-11-12 21:00:07 +0000

    Added history retrieval support to the inference pipeline

--- train/v2/flask_app.py
+++ train/v2/flask_app.py
@@ -10,6 +10,7 @@ from transformers import (
 from peft import PeftConfig, PeftModel
 
 from utils import save_conversation, load_conversation
+from zen_intent import zen_intent_classifer, prompt_from_intent
 
 from fastchat.utils import get_gpu_memory, is_partial_stop, is_sentence_complete, get_context_length
 from fastchat.conversation import get_conv_template, register_conv_template, Conversation, SeparatorStyle
@@ -18,14 +19,36 @@ from fastchat.serve.inference import generate_stream
 from flask import Flask, jsonify, request
 import streamlit as st
 
+import firebase as pyrebase
+from datetime import datetime
+
+from langchain.vectorstores import Qdrant
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+
 warnings.filterwarnings('ignore')
+
+SYSTEM_MSG = """Your name is ZenAI and you're a therapist. Please have a conversation with your patient and provide them with a helpful response to their concerns."""
+
+# Configuration Key
+firebaseConfig = {
+    'apiKey': "AIzaSyBvAeBh-ghFe-4n9VSNTSW_h9zCT3bXngg",
+    'authDomain': "cloud-lab-ff59.firebaseapp.com",
+    'projectId': "cloud-lab-ff59",
+    'databaseURL': "https://cloud-lab-ff59-default-rtdb.firebaseio.com/",
+    'storageBucket': "cloud-lab-ff59.appspot.com",
+    'messagingSenderId': "1016159354336",
+    'appId': "1:1016159354336:web:862ea0d538eee01c11ff85",
+}
+# Firebase Authentication
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
 
 
 try:
     register_conv_template(
         Conversation(
             name="ZenAI",
-            system_message="Your name is ZenAI and you're a therapist. Please have a conversation with your patient and provide them with a helpful response to their concerns.",
+            system_message=SYSTEM_MSG,
             roles=("USER", "ASSISTANT"),
             sep_style=SeparatorStyle.ADD_COLON_TWO,
             sep=" ",
@@ -35,7 +58,48 @@ try:
 except AssertionError:
     pass
 
-SYSTEM_MSG = """Your name is ZenAI and you're a therapist. Please have a conversation with your patient and provide them with a helpful response to their concerns."""
+
+def get_embeddings():
+    encode_kwargs = {'normalize_embeddings': True}
+    embeddings = HuggingFaceBgeEmbeddings(
+        model_name="BAAI/bge-base-en-v1.5",
+        encode_kwargs=encode_kwargs
+    )
+    return embeddings
+
+
+def create_vectorstore(documents):
+    embeddings = get_embeddings()
+
+    qdrant = Qdrant.from_documents(
+        documents, embeddings, 
+        location=":memory:",
+        collection_name="convo"
+    )
+
+    return qdrant
+
+
+# Function to send message to Firebase
+def send_message(user_id, conv):
+    for sender, message in conv:
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S.%f")
+        message_data = {'message': message, 'timestamp': dt_string, 'sender': sender}
+        db.child(user_id).child("Messages").push(message_data)
+
+
+# Fetch Conversation History from Firebase and sort in descending order of timestamp
+def get_chat_history(user_id):
+    messages = db.child(user_id).child("Messages").get()
+    chat_history = [{'message': message.val()['message'], 'timestamp': message.val()['timestamp'], 'sender': message.val()['sender']} for message in messages.each()] if messages.val() else []
+    chat_history = sorted(chat_history, key=lambda x: datetime.strptime(x['timestamp'], "%d/%m/%Y %H:%M:%S.%f"), reverse=False)
+    chat_history = [[x["sender"].upper(), x["message"]] for x in chat_history]
+    
+    conv = get_conv_template("ZenAI")
+    conv.set_system_message(SYSTEM_MSG)
+    conv.messages = chat_history
+    return conv
 
 
 def load_model(model_path, num_gpus, max_gpu_memory=None):
@@ -73,38 +137,54 @@ def load_model(model_path, num_gpus, max_gpu_memory=None):
 use_vicuna = False
 num_gpus = 4
 max_gpu_memory = "12GiB"
-model_path = "/home/jupyter/therapy-bot/models/zenai_sample/"
+model_path = "/home/jupyter/therapy-bot/models/ZenAI/"
 if use_vicuna:
     _, tokenizer, model = load_model(model_path, num_gpus, max_gpu_memory)
 else:
     model, tokenizer, _ = load_model(model_path, num_gpus, max_gpu_memory)
 
 
+def get_context(conv, question):
+    messages = [x[0] + ": " + x[1] for x in conv.messages]
+    documents = ['\n'.join(messages[i:i + 10]) for i in range(0, len(messages), 10)]
+    
+    vectordb = create_vectorstore(documents)
+    context = vectordb.similarity_search_with_score(question, k=1)
+    return context
+
 def chat_streamlit(
-    model, tokenizer, username, question,
+    model, tokenizer, user_id, question,
     device, num_gpus, max_gpu_memory,
     conv_template="ZenAI", system_msg=SYSTEM_MSG,
     temperature=0.7, repetition_penalty=1.0, max_new_tokens=512,
     dtype=torch.float16,
     judge_sent_end=True
 ):
-    conv_path = f"saved_conversations/{username}.json"
     context_len = get_context_length(model.config)
-
-    def new_chat():
-        conv = get_conv_template(conv_template)
-        conv.set_system_message(system_msg)
-        return conv
-
-    if os.path.exists(conv_path):
-        conv = load_conversation(conv_path)
-    else:
-        conv = new_chat()
-
+    
+    conv = get_chat_history(user_id)
+    
+    intent = zen_intent_classifer(question)
+    if intent == 4:
+        system_message = prompt_from_intent(intent)
+        return conv, system_message
+    
+    if intent == 2 or intent == 3:
+        system_message = prompt_from_intent(intent)
+        conv.set_system_message(system_message)
+    
     conv.append_message(conv.roles[0], question)
     conv.append_message(conv.roles[1], None)
     prompt = conv.get_prompt()
+    
+    if intent == 1 and len(tokenizer.encode(prompt)) > context_len - 500:
+        system_message = prompt_from_intent(intent)
+        context = get_context(question)
+        system_message += context + "\n\n"
 
+        conv.set_system_message(system_message)
+        prompt = conv.get_prompt()
+        
     gen_params = {
         "prompt": prompt,
         "temperature": temperature,
@@ -114,6 +194,7 @@ def chat_streamlit(
         "stop_token_ids": conv.stop_token_ids,
         "echo": False,
     }
+    print("---", prompt)
 
     output_stream = generate_stream(
         model,
@@ -123,11 +204,8 @@ def chat_streamlit(
         context_len=context_len,
         judge_sent_end=judge_sent_end,
     )
-    print("##############################################################################")
-    print(prompt)
-    print("##############################################################################")
     
-    return conv, output_stream, conv_path
+    return conv, output_stream
 
 
 def stream_output(output_stream):
@@ -147,22 +225,28 @@ app = Flask(__name__)
 
 def get_answer():
     question = request.json.get("question", "")
-    username = request.json.get("username", "")
-    print("******************************************", question, "******************************************")
-    conv, output_stream, conv_path = chat_streamlit(
+    user_id = request.json.get("user_id", "")
+    print("-----------------------------------------", question, "------------------------------------------")
+    conv, output_stream = chat_streamlit(
         model=model,
         tokenizer=tokenizer,
-        username="kmanish",
+        user_id=user_id,
         question=question,
         device="cuda",
         num_gpus=4,
         max_gpu_memory="12GiB"
     )
-    answer = stream_output(output_stream).strip()
-    print("******************************************", answer, "******************************************")
+    if isinstance(output_stream, str):
+        answer = output_stream
+    else:
+        answer = stream_output(output_stream).strip()
     conv.update_last_message(answer)
-    save_conversation(conv, conv_path)
     
+    print("##############################################################################")
+    print(conv.get_prompt())
+    print("##############################################################################")
+    
+    send_message(user_id, conv.messages[-2:])
     return answer
     
 
